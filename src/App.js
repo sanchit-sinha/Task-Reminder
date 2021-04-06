7e8f80ed7e314dd021333b532ec15f4ef7309ef7
sanchit-sinha <sanchit4u2001@gmail.com>
2021-04-06 10:44:34 +0530

    update delete and set reminder of tasks

--- src/App.js
+++ src/App.js
@@ -18,10 +18,22 @@ function App() {
     }
   ])
 
+  //delete task 
+  const deleteTask = (id) => {
+    // console.log('delete' , id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // toggle reminder
+  const toggleReminder=(id) => {
+    // console.log(id)
+    setTasks(tasks.map((task) => task.id === id ? {...task , reminder : !task.reminder} : task))
+  }
+
   return (
     <div className = "container">
       <Header />
-      <Tasks tasks = {tasks} />
+      {tasks.length > 0 ? <Tasks tasks = {tasks}  onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,10 @@
-const Tasks = ({tasks}) => {
+import Task from './Task'
+const Tasks = ({tasks , onDelete , onToggle}) => {
     return (
         <>
-        {/* for each task in tasks , do the folloeing (unique key value) */}
+        {/* for each task in tasks , do the following (unique key value) */}
           {tasks.map((task) => (
-              <h3 key={task.id}>{task.text}</h3>
+              <Task key={task.id} task = {task} onDelete = {onDelete} onToggle = {onToggle}/>
           ))}  
         </>
     )
